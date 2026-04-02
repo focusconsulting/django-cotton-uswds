f0315b3c15671ca773d3e35ca3d49534036cc539
Iverson Diles <ivcotad@gmail.com>
2026-04-02 10:17:46 -0700

    feat: promote USWDSFormMixin and USWDSFormRenderer to package root
    Re-exports both classes from __init__.py with __all__ so callers can use
    `from django_cotton_uswds import USWDSFormMixin/USWDSFormRenderer` and
    `FORM_RENDERER = "django_cotton_uswds.USWDSFormRenderer"`. Deep import
    paths remain functional for backward compatibility.
    
    Closes #11

--- django_cotton_uswds/__init__.py
+++ django_cotton_uswds/__init__.py
@@ -3,3 +3,8 @@ try:
     from django_cotton_uswds._version import __version__
 except ImportError:
     __version__ = "0.0.0.dev0"
+
+from django_cotton_uswds.mixins import USWDSFormMixin
+from django_cotton_uswds.renderer import USWDSFormRenderer
+
+__all__ = ["USWDSFormMixin", "USWDSFormRenderer"]
