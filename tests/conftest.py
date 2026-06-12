b7fd983651305eb0ba5a8372decf75676ffa6cc2
Mohammad Taleb <t@lebbits.com>
2026-06-12 15:04:23 -0400

    style: tidy test conftest (drop unused import, format)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import django
 from django.conf import settings
 
 
@@ -7,6 +6,7 @@ def pytest_configure():
     for backend in settings.TEMPLATES:
         if backend["BACKEND"] == "django.template.backends.django.DjangoTemplates":
             import os
+
             tests_dir = os.path.dirname(__file__)
             backend["DIRS"].append(os.path.join(tests_dir, "templates"))
             break
